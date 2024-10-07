628d124348a0970d22c3ad15f57df3214cbb8fff
Коммерческие курсы <comcuru@teach.local>
2024-10-07 13:21:56 +0300

    Контрольная точка

---
+++ WpfApp1/WpfApp1/Pages/ListViewPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ListViewPage.xaml
+    /// </summary>
+    public partial class ListViewPage : Page
+    {
+        public ListViewPage()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        private void Init()
+        {
+            ListViewBox.ItemsSource = Data.KT04Entities.GetContext().Users.ToList();
+        }
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
